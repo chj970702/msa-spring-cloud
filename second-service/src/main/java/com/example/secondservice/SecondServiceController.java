41663c084d946bd55d9c0d3ee0fd4439c29b8d0a
hyukjin <cdh5771@naver.com>
2023-10-13 22:27:22 +0900

    feat: request & response 확인 및 api 추가

--- second-service/src/main/java/com/example/secondservice/SecondServiceController.java
+++ second-service/src/main/java/com/example/secondservice/SecondServiceController.java
@@ -1,15 +1,29 @@
 package com.example.secondservice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/second-service")
 public class SecondServiceController {
     @GetMapping("/welcome")
     public String welcome() {
         return "Welcome to second service";
+    }
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("second-request") String header) {
+        log.info(header);
+        return "Second service header message";
+    }
+
+    @GetMapping("/check")
+    public String check() {
+        return "This is a message from second Service";
     }
 }
 
